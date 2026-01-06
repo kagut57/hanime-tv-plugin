24b3e98ff126608c0db5e199b8be0f408b446530
kagut57 <156945786+kagut57@users.noreply.github.com>
2026-01-06 23:22:58 +0530

    Update htv.py

--- src/yt_dlp_plugins/extractor/htv.py
+++ src/yt_dlp_plugins/extractor/htv.py
@@ -1,32 +1,20 @@
 import re
 
 from yt_dlp.extractor.common import InfoExtractor
-from yt_dlp.utils import traverse_obj, str_or_none, int_or_none, url_or_none, urljoin, clean_html, OnDemandPagedList
+from yt_dlp.utils import (
+    traverse_obj,
+    str_or_none,
+    int_or_none,
+    url_or_none,
+    urljoin,
+    clean_html,
+    OnDemandPagedList,
+)
+
 
 class HanimeTVIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?hanime\.tv/(videos/hentai|hentai/video)/(?P<id>[a-z0-9\-]+)'
-    _TESTS = [{
-        'url': 'https://hanime.tv/videos/hentai/itadaki-seieki',
-        'md5': '68a3fbb672229cecd12b52528168e6ff',
-        'info_dict': {
-            'id': 'itadaki-seieki',
-            'title': 'Itadaki! Seieki',
-            'creator': 'Pashmina',
-            'description': str,
-            'duration': 1378,
-            'upload_date': '20160607',
-            'release_date': '20140327',
-            'timestamp': 1465257759,
-            'release_timestamp': 1395932400,
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'series': 'Itadaki! Seieki',
-            'view_count': int,
-            'like_count': int,
-            'tags': list,
-            'dislike_count': int,
-            'ext': 'mp4',
-        },
-    }]
+    _VALID_URL = r'https?://(?:www\.)?hanime\.tv/(?:videos/hentai|hentai/video)/(?P<id>[a-z0-9\-]+)'
+    _IE_NAME = 'hanimetv'
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
@@ -37,6 +25,9 @@ class HanimeTVIE(InfoExtractor):
             description = clean_html(video.get('description'))
 
             return {
+                '_type': 'url',
+                'url': urljoin('https://hanime.tv/videos/hentai/', video.get('slug')),
+                'ie_key': HanimeTVIE.ie_key(),
                 'id': video.get('slug'),
                 'title': video.get('name'),
                 'creator': video.get('brand'),
@@ -46,41 +37,63 @@ class HanimeTVIE(InfoExtractor):
                 'description': description,
                 'view_count': int_or_none(video.get('views')),
                 'like_count': int_or_none(video.get('likes')),
-                'tags': [tag.get('text') for tag in video.get('hentai_tags', [])],
                 'dislike_count': int_or_none(video.get('dislikes')),
+                'tags': [t.get('text') for t in video.get('hentai_tags', [])],
                 'thumbnail': url_or_none(video.get('poster_url')),
-                'series': traverse_obj(page, ('hentai_franchise', 'title'), expected_type=str),
-                'ext': 'mp4',
+                'series': traverse_obj(page, ('hentai_franchise', 'title')),
             }
 
-        page = self._download_json('https://hanime.tv/api/v8/video', None, query={'id': video_id})
+        page = self._download_json(
+            'https://hanime.tv/api/v8/video',
+            None,
+            query={'id': video_id},
+        )
+
         franchise = traverse_obj(page, ('hentai_franchise'), default={}, expected_type=dict)
-        franchise_videos = traverse_obj(page, ('hentai_franchise_hentai_videos'), default=[], expected_type=list)
+        franchise_videos = traverse_obj(
+            page,
+            ('hentai_franchise_hentai_videos'),
+            default=[],
+            expected_type=list,
+        )
 
         entries.append(video_result(page))
 
-        is_franchise = self._configuration_arg('franchise', [False], ie_key=HanimeTVIE.ie_key())[0]
+        is_franchise = self._configuration_arg(
+            'franchise',
+            [False],
+            ie_key=HanimeTVIE.ie_key(),
+        )[0]
+
         if is_franchise:
             for entry in franchise_videos[1:]:
                 page = self._download_json(
                     'https://hanime.tv/api/v8/video',
                     None,
-                    query={'id': str_or_none(entry.get('slug'))}
+                    query={'id': str_or_none(entry.get('slug'))},
                 )
                 entries.append(video_result(page))
 
-        return self.playlist_result(entries, playlist_title=str_or_none(franchise.get('title')))
+        return self.playlist_result(
+            entries,
+            playlist_id=video_id,
+            playlist_title=str_or_none(franchise.get('title')),
+        )
 
 
 class HanimeTVPlaylistIE(InfoExtractor):
-    _VALID_URL = r"https?://(?:www\.)?hanime\.tv/playlists/(?P<id>[a-z0-9\-]+)"
+    _VALID_URL = r'https?://(?:www\.)?hanime\.tv/playlists/(?P<id>[a-z0-9\-]+)'
+    _IE_NAME = 'hanimetv:playlist'
 
     def _real_extract(self, url):
         playlist_id = self._match_id(url)
 
         num_page_ents = int_or_none(
-            self._configuration_arg('num_page_ents', ie_key=HanimeTVIE.ie_key()),
-            default=24
+            self._configuration_arg(
+                'num_page_ents',
+                ie_key=HanimeTVIE.ie_key(),
+            ),
+            default=24,
         )
 
         def fetch_page(pagenum):
@@ -92,24 +105,32 @@ class HanimeTVPlaylistIE(InfoExtractor):
                     'playlist_id': playlist_id,
                     '__order': 'sequence,DESC',
                     '__offset': pagenum * num_page_ents,
-                    '__count': num_page_ents
-                }
+                    '__count': num_page_ents,
+                },
             )
 
-            data = traverse_obj(page, ('fapi', 'data'), default=[], expected_type=list)
+            data = traverse_obj(
+                page,
+                ('fapi', 'data'),
+                default=[],
+                expected_type=list,
+            )
 
             return [
                 self.url_result(
-                    urljoin('https://hanime.tv/videos/hentai/', entry.get('slug')),
-                    video_id=entry.get('id'),
-                    video_title=entry.get('name'),
-                    ie_key=HanimeTVIE.ie_key()
+                    urljoin('https://hanime.tv/videos/hentai/', e.get('slug')),
+                    ie_key=HanimeTVIE.ie_key(),
+                    video_id=e.get('slug'),
+                    video_title=e.get('name'),
                 )
-                for entry in data
+                for e in data
             ]
 
-        return self.playlist_result(OnDemandPagedList(fetch_page, num_page_ents), playlist_id)
-
+        return self.playlist_result(
+            OnDemandPagedList(fetch_page, num_page_ents),
+            playlist_id,
+        )
+        
 # import re
 
 # from yt_dlp.extractor.common import InfoExtractor
